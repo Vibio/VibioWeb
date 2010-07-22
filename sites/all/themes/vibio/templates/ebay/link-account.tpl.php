43cdf0b0aac3336ffc532d293d17ed2a769fb468
Nelson Monterroso <nelson@vibio.com>
2010-07-22 14:17:20 -0700

    changed ebay account linking dialog to open in the same window, rather than in a new one.

--- sites/all/themes/vibio/templates/ebay/link-account.tpl.php
+++ sites/all/themes/vibio/templates/ebay/link-account.tpl.php
@@ -2,4 +2,4 @@
 $login_url = _ebay_get_login_url()."&RuName=".variable_get("ebayapi_runame", "some_runame")."&SessID=$session_id";
 ?>
 Linking accounts is what cool kids do.
-<a href="<?php echo $login_url; ?>" target="_blank">Do it!</a>
+<a href="<?php echo $login_url; ?>">Do it!</a>

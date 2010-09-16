d46d67967ba6e37f0657691de5dd038bfc97da31
Nelson Monterroso <nelson@vibio.com>
2010-09-15 18:08:16 -0700

    added break after "view yours" when viewing an item

--- themes/vibio/templates/product/inventory-manage-link.tpl.php
+++ themes/vibio/templates/product/inventory-manage-link.tpl.php
@@ -6,7 +6,7 @@ $is_product_page = !isset($product->item);
 if ($is_product_page && $item_id || // PRODUCT page where user owns the product
 	(!$is_product_page && $item_id && $product->item->uid != $user->uid)) // ITEM page where the current user owns the product, but isn't looking at their own item page
 {
-	$manage_link = l(t("View yours"), "node/$item_id");
+	$manage_link = l(t("View yours"), "node/$item_id")."<br />";
 }
 elseif (!$item_id) // user doesn't own this product, always show this.
 {

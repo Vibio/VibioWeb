d6297396fe9af424c42280ccc34374a0166f4d8a
Nelson Monterroso <nelson@vibio.com>
2010-09-20 18:03:31 -0700

    fixed bug where cache_set, cache_get weren't returning the parent's result, so caching wasn't being used for simple argument caching. also hover text on facebook share icons.

--- themes/vibio/templates/facebook/share.tpl.php
+++ themes/vibio/templates/facebook/share.tpl.php
@@ -1,9 +1,10 @@
 <?php
+$alt = t("Share on Facebook");
 echo "
 	<div class='fb_share_container'>
 		<span class='fb_share_params'>$share_params</span>
 		<a class='fb_share' href='#'>
-			<img src='/themes/vibio/images/facebook/share.png' />
+			<img src='/themes/vibio/images/facebook/share.png' alt='$alt' title='$alt' />
 		</a>
 	</div>
 ";

b983d3fc2071e0d2ef6391818ed03c1bc1ddea5e
Nelson Monterroso <nelson@vibio.com>
2010-09-22 14:51:12 -0700

    make profile block items the same height and give hover titles

--- themes/vibio/templates/profile/display-block-item.tpl.php
+++ themes/vibio/templates/profile/display-block-item.tpl.php
@@ -1,8 +1,8 @@
 <?php
 echo "
 	<div class='profile_ext_displayblock_item'>
-		<a href='{$item_data['href']}'>
-			<img src='{$item_data['image']}' />
+		<a href='{$item_data['href']}' alt='{$item_data['title']}' title='{$item_data['title']}'>
+			<img src='{$item_data['image']}' alt='{$item_data['title']}' title='{$item_data['title']}' />
 			<span>{$item_data['count']}</span>
 		</a>
 	</div>

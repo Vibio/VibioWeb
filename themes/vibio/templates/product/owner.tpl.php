5388aa4a3b6d91b72b829b8c115770108d1791dd
Nelson Monterroso <nelson@vibio.com>
2010-09-29 21:25:34 +0000

    styled the list of users who also have the same item in your network/vibio

--- themes/vibio/templates/product/owner.tpl.php
+++ themes/vibio/templates/product/owner.tpl.php
@@ -1,15 +1,12 @@
 <?php
-$dos = !empty($item['user']['dos']) ? "<xmp style='float: right;'>".print_r($item['user']['dos'], true)."</xmp>" : "";
 echo "
 	<div class='item_owner'>
-		<a href='/user/{$item['user']['uid']}'>
-			<img src='{$item['user']['picture']}' title='{$item['user']['name']}' />
-		</a>
-		<span class='item_info'>
+		{$item['user']['picture']}
+		<div class='item_info'>
 			{$item['node']}
-		</span>
-		$dos
+			{$item['user']['dos']}
+		</div>
 	</div>
-	<div style='clear: both;'></div>
+	<div class='clear'></div>
 ";
 ?>

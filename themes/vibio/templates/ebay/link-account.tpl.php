4c3878fa1b9a0362cb56a1807467a07141d56f01
Nelson Monterroso <nelson@vibio.com>
2010-09-29 22:00:18 +0000

    Changed text in ebay link account popup

--- themes/vibio/templates/ebay/link-account.tpl.php
+++ themes/vibio/templates/ebay/link-account.tpl.php
@@ -1,5 +1,4 @@
 <?php
 $login_url = _ebay_get_login_url()."&RuName=".variable_get("ebayapi_runame", "some_runame")."&SessID=$session_id";
 ?>
-Linking accounts is what cool kids do.
-<a href="<?php echo $login_url; ?>">Do it!</a>
+<a href="<?php echo $login_url; ?>">Link your eBay and Vibio accounts!</a>

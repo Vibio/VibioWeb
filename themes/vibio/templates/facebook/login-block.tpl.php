c3dd9c2d704a0a59254ecdb8b83a7ce11332264d
Nelson Monterroso <nelson@vibio.com>
2010-10-03 03:12:26 +0000

    styled fb login block and user login block

--- themes/vibio/templates/facebook/login-block.tpl.php
+++ themes/vibio/templates/facebook/login-block.tpl.php
@@ -3,7 +3,12 @@ global $user;
 
 if (!$user->uid)
 {
+	$title = t("New to Vibio?");
+	$description = t("You can log in with your Facebook account");
+
 	echo "
+		<h5 class='menu_header'>$title</h5>
+		<span class='menu_description'>$description</span>
 		<div class='fb_login'>
 			<img class='fb_login_button' src='/themes/vibio/images/facebook/login-button.png' />
 		</div>

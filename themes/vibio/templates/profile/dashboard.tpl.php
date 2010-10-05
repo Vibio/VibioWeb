8cd56f21e3d94c97a8b22dd8f98670059481220b
Nelson Monterroso <nelson@vibio.com>
2010-10-05 22:49:00 +0000

    added initial login calls to action

--- themes/vibio/templates/profile/dashboard.tpl.php
+++ themes/vibio/templates/profile/dashboard.tpl.php
@@ -13,6 +13,34 @@ foreach ($profile_tabs as $title => $tab)
 	";
 	$tab_content .= "<div id='$tab_id'>{$tab}</div><div class='clear'></div>";
 }
+
+$action_html = "";
+if (!empty($profile_actions))
+{
+	$new_header = t("Looks like you're new");
+	$new_helptext = t("Here are some things to help you get started...");
+	$action_html .= "<div id='profile_call_to_action'>";
+	$action_html .= "<h3>$new_header</h3>";
+	$action_html .= "<span>$new_helptext</span>";
+
+	$action_html .= "<table>";
+	foreach ($profile_actions as $action)
+	{
+		$action_html .= "
+			<tr>
+				<td class='profile_action_image'>
+		";
+		if ($action['image'])
+			$action_html .= "<img src='{$action['image']}' />";
+		$action_html .= "</td>";
+		$action_html .= "<td class='profile_action'>{$action['text']}</td>";
+	}
+
+	$action_html .= "</table></div>";
+}
+
+
+echo $action_html;
 ?>
 
 <div id='profile_tabs' class='tabs'>

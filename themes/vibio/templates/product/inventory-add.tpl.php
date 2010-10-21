28e56546f7bea93d1efda9a657c2bcf17f976b17
Nelson Monterroso <nelson@vibio.com>
2010-10-21 15:44:44 -0700

    [finishes #5616911]

--- themes/vibio/templates/product/inventory-add.tpl.php
+++ themes/vibio/templates/product/inventory-add.tpl.php
@@ -1,12 +1,8 @@
 <?php
-$link_params = array("attributes" => array("class" => "inventory_add", "id" => "inventory_add_{$nid}"));
-
-if ($searchcrumb)
-{
-	$link_params["query"] = "searchcrumb=$searchcrumb";
-}
-
-$add = l("I have one", "product/{$nid}/add-to-inventory", $link_params);
-
-echo "$add<br />";
+$text = t("I have one!");
+echo "
+	<button class='inventory_add' id='inventory_add_{$nid}'>
+		$text
+	</button><br />
+";
 ?>

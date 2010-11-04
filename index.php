f7fdfc2de7be21490d6ecaafd0793e437e57bbd3
Nelson Monterroso <nelson@vibio.com>
2010-11-04 10:49:19 -0700

    [finishes #5987150] - added badge type for being on the site for X days. Also added special triggers page that describes what special triggers are available.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  */
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-//menu_rebuild();
+menu_rebuild();
 $return = menu_execute_active_handler();
 
 // Menu status constants are integers; page content is a string.

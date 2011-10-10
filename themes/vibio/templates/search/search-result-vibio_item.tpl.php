1b32d677d9c716f9cf7763a4b448c19707786c89
root <root@ip-10-244-151-207.ec2.internal>
2011-10-10 18:26:13 +0000

    dev.vibio appears ready after quick sweep

--- themes/vibio/templates/search/search-result-vibio_item.tpl.php
+++ themes/vibio/templates/search/search-result-vibio_item.tpl.php
@@ -1,9 +1,6 @@
 <?php /* print '<pre>'. check_plain(print_r($info_split, 1)) .'</pre>'; */ 
-//print $result[node]->nid;
 $node = $result[node]; //!!! Different from node-view-flag_featured.tpl.php
 		// !!! Also, $search_links is set here, not there.
-//dpm(debug_backtrace());
-//print_r(debug_backtrace());
 $flag = flag_create_link('feature', $node->nid);
 $img = theme('imagecache', 'product_fixed_width', $node->field_main_image[0]['filepath']);
 

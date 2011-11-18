47870481d10966d0d0b6a8a9b73292c764c38fd4
root <root@ip-10-124-101-90.ec2.internal>
2011-11-18 20:33:29 +0000

    last minute sidebar fixx

--- themes/vibio/js/fixed-scroll.js
+++ themes/vibio/js/fixed-scroll.js
@@ -1,7 +1,7 @@
 /*fixed scroll*/
 $(document).ready(function() {
     $('#block-block-5').scrollToFixed();
-    $('#block-block-5').bind('fixed', function() { $(this).css('margin', '50px 0px 0'); });
+    $('#block-block-5').bind('fixed', function() { $(this).css('margin', '10px 0px 0'); });
     $('#block-block-5').bind('unfixed', function() { $(this).css('margin', '50px -10px 0'); });
     //$('.view.view-flag-featured .view-footer').scrollToFixed();
 });

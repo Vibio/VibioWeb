ab7b82df6b79e302c94b003349938e4cd3202d42
Alec <reynolds.alec@gmail.com>
2011-11-28 19:55:25 +0000

    Reverted search results images back to imagecached

--- themes/vibio/templates/search/search-result-vibio_item.tpl.php
+++ themes/vibio/templates/search/search-result-vibio_item.tpl.php
@@ -3,18 +3,8 @@ $node = $result[node]; //!!! Different from node-view-flag_featured.tpl.php
 		// !!! Also, $search_links is set here, not there.
 $flag = flag_create_link('feature', $node->nid);
 //dsm($node->field_main_image[0]['filepath']);
-//Alec hack: turn off imagecache
-$img_path = $node->field_main_image[0]['filepath'];
-if(strstr($img_path, 'sites') != FALSE){
-	//This handles images saved from Amazon, possibly newly uploaded images as well
-	$img = theme('image', $img_path, '', '', array('width' => 160), FALSE);
-}else{
-	//This handles uploaded images (possibly only old ones). I believe
-	//these images have file paths like /var/www/..., as per the
-	//the old filesystem. Perhaps we need to clear out the old file paths?
-	$img_path = 'http://staging.vibio.com/sites/default/files/uploads/' . $img_path; 
-	$img = theme('image', $img_path, '', '', array('width' => 160), FALSE);
-}
+$img = theme('imagecache', 'product_fixed_width', $node->field_main_image[0]['filepath']);
+
 // $mine should be true if it's yours,  true or null
 // Fires for PRODUCTS (merely searched products too)
 //if ($node->thisismine) { $minetext = "It's mine all mine"; }

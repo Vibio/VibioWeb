fbea25c7b1c8b0c270425c48755593401f855c3c
Alec <reynolds.alec@gmail.com>
2011-12-02 21:00:18 +0000

    Hotfix for badges on the what badges can I earn popup

--- themes/vibio/templates/badge/alert-badge-single.tpl.php
+++ themes/vibio/templates/badge/alert-badge-single.tpl.php
@@ -1,5 +1,5 @@
 <div class="badge_alert_single">
-	<?php echo $badge->image; ?>
+	<?php echo $badge_image; ?>
 	<div class="badge_title"><?php echo $badge->title; ?></div>
 	<!--<div class="badge_text"><?php echo $badge->description; ?></div>-->
 </div>

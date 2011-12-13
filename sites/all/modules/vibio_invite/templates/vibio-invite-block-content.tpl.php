0f6e3a4ebff09cacbe3d3e79424060fa66412c58
Alec Reynolds <reynolds.alec@gmail.com>
2011-12-13 14:52:23 -0800

    vibio_invite first draft, non-working

---
+++ sites/all/modules/vibio_invite/templates/vibio-invite-block-content.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Available variables:
+ * $fb_url (note that this must have an id of fb-request to work
+ * $twitter_url
+ * $email_url
+ */
+?>
+
+<ul>
+  <li>
+    <a href="<?php print $fb_link; ?>" id="fb-request">
+      <img src="sites/all/themes/images/fb-invite.jpg" />
+    </a>
+  </li>
+  <li>
+    <a href="<?php print $twitter_link; ?>">
+      <img src="sites/all/themes/images/twitter-invite.jpg" />
+    </a>
+  </li>
+  <li>
+    <a href="<?php print $email_link; ?>">
+      <img src="sites/all/themes/images/email-invite.jpg" />
+    </a>
+  </li>
+</ul>
+<p>cheese</p>
+
+
+
+

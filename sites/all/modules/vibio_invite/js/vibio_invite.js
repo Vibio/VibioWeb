8a2eb9868923cec4d08c9a39d518b08c85d994a8
Alec Reynolds <reynolds.alec@gmail.com>
2011-12-16 08:47:48 -0800

    Edits to the apprequest dialog on the vibio_invite module

--- sites/all/modules/vibio_invite/js/vibio_invite.js
+++ sites/all/modules/vibio_invite/js/vibio_invite.js
@@ -1,37 +1,31 @@
-(function ($) {
-  Drupal.behaviors.fbshare = {
-    attach: function(context, setttings){
+if(Drupal.jsEnabled){
+  $(document).ready(function(){
+    //The share button is clicked, initiating the FB dialog
+    $('a#fb-request').click(function (event) {
+      event.preventDefault();
+      var $this = $(this);
+      //Get the inviter's username
+      var username = $this.attr('username');
+      invite(username);
+    });
 
-//The share button is clicked, initiating the FB dialog
-$('a#fb-request').click(function (event) {
-  event.preventDefault();
-  var $this = $(this);
-  //Get properties from the link. Assumes the custom nid attribute has been
-  //set
-  var link = $this.attr('href');
-  var username = $this.attr('username');
-  invite(link, username);
-});
-
-//Invite a user to vibio using FB Request Dialog
-//See http://developers.facebook.com/docs/reference/dialogs/requests/
-function invite(link, username, uid) {
-    //Facebook request dialog initiation.
-    FB.ui(
-     {
-      method: 'apprequests',
-      message: "See " + username + "'s collections and share your stuff on Vibio",
-      //passes along the uid; this can be used by the FB apps canvas page
-      //to redirect to the inviting user's profile page
-      data: uid,
-      display: 'popup'
-     },
-     function(response){
-       console.log(response);
-     }
-    );
+    //Invite a user to vibio using FB Request Dialog
+    //See http://developers.facebook.com/docs/reference/dialogs/requests/
+    function invite(username) {
+        //Facebook request dialog initiation.
+        FB.ui(
+         {
+          method: 'apprequests',
+          message: "Join " + username + " on Vibio to share your stuff and see your friends' collections.",
+          //passes along the uid; this can be used by the FB apps canvas page
+          //to redirect to the inviting user's profile page
+          display: 'popup'
+         },
+         function(response){
+           console.log(response);
+         }
+        );
+    }
+  });
 }
 
-}
-}
-})(jQuery);

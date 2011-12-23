54049e17ebab4c8fc57c8cd4846f4c873d7b7be4
Alec Reynolds <reynolds.alec@gmail.com>
2011-12-22 17:35:52 -0800

    Placing og:image data on the front page

--- themes/vibio/template.php
+++ themes/vibio/template.php
@@ -1,6 +1,24 @@
 <?php
 
 /**
+ * Adds OG meta data for the front page, effectively representing Vibio to FB.
+ * 
+ * @param <type> $variables
+ */
+function vibio_preprocess_page(&$variables){
+  if(drupal_is_front_page()){
+    global $base_url;
+    $site_image = $base_url . '/'. drupal_get_path('theme', 'vibio') . '/vibio-logo.png';
+    $og_image = '<meta property="og:image" content="' . $site_image . '"/>';
+    drupal_set_html_head($og_image);
+    
+    //This is apparently necessary, see http://api.drupal.org/api/drupal/includes--common.inc/function/drupal_set_html_head/6#comment-4614
+    $variables['head'] = drupal_get_html_head();
+
+  }
+}
+
+/**
 * Adds OG meta data to share an appropriate collection picture with Facebook.
 * Note that vibio_addthis module contains the exact same functionality for vibio
 * product nodes. It'd be great to combine all this functionality in one module, or,

ac2741646c9f50f49c81a0ae31219dfa3d4a8149
Alec Reynolds <reynolds.alec@gmail.com>
2012-01-04 14:12:14 -0800

    Refactoring of the control and presentational layers of the collections system.
    -Replicates the old system, but uses the new data models

--- themes/vibio/templates/views/user_collections1/views-view-unformatted--user-collections1.tpl.php
+++ themes/vibio/templates/views/user_collections1/views-view-unformatted--user-collections1.tpl.php
@@ -10,9 +10,15 @@ echo "</div>";
 foreach ($view->result as $collection)
 {
   /*
-   * The $collection item is passed in w/ node_title and nid populated. Other
-   * variables will be prepopulated in collection_preprocess_collection_list_item
+   * Reformat the $collection object to match the standard node format
+   * that collection.tpl.php anticipates. Additional values will be
+   * populated in collection_preprocess_collection_list_item().
+   * @todo: consider moving into a views preprocess for user_collections1
    */
+  $collection->title = $collection->node_title;
+  $collection->name = $collection->users_name;
+  $collection->uid = $collection-users_uid;
+  $collection->body = $collection->node_revisions_body;
 	echo theme("collection_list_item", $collection, $view->args[1]);
 }
 ?>

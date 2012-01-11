2ce4172978acc4c829a93234afbd08811337a606
Alec Reynolds <reynolds.alec@gmail.com>
2012-01-11 15:08:02 -0800

    Created first draft of the related items feature, fixed a bug with collections creation.

---
+++ themes/vibio/templates/collections/related-item.tpl.php
@@ -0,0 +1,17 @@
+<?php
+$image = field_file_load($item->node_data_field_main_image_field_main_image_fid);
+$link = l(t("Check it out"), "collections/{$item->nid}", array("attributes" => array("class" => "collection_link")));
+$collection_image = theme('imagecache', 'collection_fixed_fluid_grid_115', $image['filepath'], $item->node_title, $item->node_title, '');
+echo "
+	<div class='collection_image'>
+		<a href='/node/{$item->nid}'>
+			{$collection_image}
+		</a>
+	</div>
+	<div class='collection_info'>
+		<div class='collection_owner'>{$item->users_name}</div>
+		<div class='collection_title'>{$item->node_title}</div>
+		<div class='collection_link'>$link</div>
+	</div>
+";
+?>
--- themes/vibio/templates/collections/sidebar-item.tpl.php
+++ themes/vibio/templates/collections/sidebar-item.tpl.php
@@ -1,7 +1,7 @@
 <?php
-$collection_image = theme('imagecache', 'collection_sidebar_fixed_25', $collection_item->image, $collection_item->title, $collection_item->title, '');
+$collection_image = theme('imagecache', 'collection_sidebar_fixed_25', $collection_item->field_main_image[0]['filepath'], $collection_item->title, $collection_item->title, '');
 echo "
-	<a href='/node/{$collection_item->item_nid}'>
+	<a href='/node/{$collection_item->nid}'>
 		{$collection_image}
 	</a>
 ";

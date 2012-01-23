e348aba366874109a55699d22f5d9ac92c684daa
Craig Tockman <craigtockman@gmail.com>
2012-01-23 14:06:21 -0800

    changed to related block

--- themes/vibio/js/dom-removal.js
+++ themes/vibio/js/dom-removal.js
@@ -2,5 +2,5 @@
  * @author Craig Tockman - Removing Elements from the DOM
  */
 $(document).ready(function() {
-	$('#block-collection-random_collection h2.title,.front.logged-in #block-block-3,friend_actions, #profile_progressbar, .profile_progress, div#block-collection-random_collection .collection_item_count ,  div#block-collection-random_collection .collection_description ,  div#block-collection-random_collection .collection_categories,  div#block-collection-random_collection .collection_date_added,  div#block-collection-random_collection .collection_link, .heartbeat_times, .heartbeat-buttons, .heartbeat-more-messages-wrapper, #user-login-form li.first').remove();
+	$('#block-collection-related_collection h2.title,.front.logged-in #block-block-3,friend_actions, #profile_progressbar, .profile_progress, div#block-collection-random_collection .collection_item_count ,  div#block-collection-random_collection .collection_description ,  div#block-collection-random_collection .collection_categories,  div#block-collection-random_collection .collection_date_added,  div#block-collection-random_collection .collection_link, .heartbeat_times, .heartbeat-buttons, .heartbeat-more-messages-wrapper, #user-login-form li.first').remove();
 });

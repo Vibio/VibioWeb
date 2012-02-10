c087ac6675b03f4611d090e19a722e049c2a43eb
Kevin Kaland <kevin@wizonesolutions.com>
2012-02-10 11:18:10 -0800

    Final tweaks for homepage smoothness.

--- themes/vibio/js/homepage.js
+++ themes/vibio/js/homepage.js
@@ -4,8 +4,23 @@ Drupal.behaviors.homepageFilter = function(context) {
     $(this).parents('form:eq(0)').submit();
   });
   // Update the form action appropriately when the tab changes
-  $('#quicktabs-homepage-tabs li[class^="qtab"]').click(function() {
-    $form = $(this).parents('form:eq(0)'); 
+  $('#quicktabs-homepage_tabs li a').click(function() {
+    $form = $('#vibio-product-displays-filter-form');
+    // Sorry, other query string parameters, but I don't care about you.
+    $form.attr('action', $(this).attr('href'));
+  });
+}
 
+// http://paste.pocoo.org/show/548965
+Drupal.behaviors.refreshableTabs = function(context) {
+  var pathname = window.location.pathname;
+  // Enable tab memory for ALL quicktabs.
+  var tablocator = 'quicktabs_wrapper';
+  var tab = $.cookie(pathname + tablocator);
+  if (tab != '') {
+    $('div.' + tablocator + ' ul.quicktabs_tabs a#' + tab).click();
+  }
+  $('div.' + tablocator + ' ul.quicktabs_tabs a').click(function() {
+    $.cookie(pathname + tablocator, $(this).attr('id'));
   });
 }

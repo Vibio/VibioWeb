9a89532b24cd848469aee45c0e4eb7d2b9da2525
Craig Tockman <craigtockman@gmail.com>
2012-02-10 21:34:52 -0800

    lots of js changes to product-item.js and css changes

--- themes/vibio/js/product-item.js
+++ themes/vibio/js/product-item.js
@@ -1,79 +1,91 @@
-$(document).ready(function () {
-  //only apply to the multistep pages
-  if($('body.page-product-new-item').is('*') || $('body.page-product-new').is('*')
-    || $('body.page-product-new-product').is('*')){
+$(document).ready(function() {
+	//only apply to the multistep pages
 
-    var textarea = $('#edit-body-wrapper textarea.form-textarea');
-    var description = $('#edit-body-wrapper .description');
-    if(textarea.val() == ''){
-      description.show();
-    }else{
-      description.hide();
-    }
+	if($('body.page-product-new-item').is('*') || $('body.page-product-new').is('*') || $('body.page-product-new-product').is('*')) {
+		//moving stuff around to match comps
 
-    textarea.focus(function(){
-      description.hide();
-    });
+		//move textarea above categories select on product/new page
+		$('.page-product-new .body-field-wrapper').insertBefore('#edit-taxonomy-2-wrapper');
+		//change text in the amazon product number label
+		$('.page-product-new #edit-field-amazon-asin-0-asin-wrapper label').html('ASIN:');
+		//place cancel button to the left of next button
+		$('.page-product-new #edit-cancel').insertBefore('#edit-next');
+		//place prices input and forsale input after the description textarea
+		$('.page-product-new-item #form-price').insertAfter('.body-field-wrapper');
+		$('.page-product-new-item #edit-privacy-setting-wrapper').insertBefore('#field-images-items');
 
-    description.click(function(){
-      description.hide();
-      textarea.focus();
-    });
+		var textarea = $('#edit-body-wrapper textarea.form-textarea');
+		var description = $('#edit-body-wrapper .description');
+		if(textarea.val() == '') {
+			description.show();
+		} else {
+			description.hide();
+		}
 
-    textarea.blur(function(){
-      if(textarea.val() == ''){
-        description.show();
-      }
-    });
-    
-    //only apply to the item step
-    if($('body.page-product-new-item').is('*')){
-      var initRadioValue = $('input[name=have_want]:checked').val();
+		textarea.focus(function() {
+			description.hide();
+		});
 
-      //If we're in the "want" mode
-      if(initRadioValue == 1){
-        //...hide elements
-        $('#edit-field-main-image-0-ahah-wrapper, ' +
-          '#edit-field-posting-type-value-wrapper').hide();
-        $('div#field-images-items').css('display', 'none');
-        $('div#edit-o2b-price-wrapper').parent().hide();
-        //Set the body description
-        description.html('Why do you want this item?');
-      }
+		description.click(function() {
+			description.hide();
+			textarea.focus();
+		});
 
-      $('div.form-radios').click(function (){
-        var radioValue = $('input[name=have_want]:checked').val();
-        //If it's a want form...
-        if(radioValue == 1){
-          //...hide elements
-          $('#edit-field-main-image-0-ahah-wrapper, div#field-images-items, ' +
-            '#edit-field-posting-type-value-wrapper').hide();
-          $('div#field-images-items').css('display', 'none');
-          $('div#edit-o2b-price-wrapper').parent().hide();
-          description.html('Why do you want this item?');
-        }else{
-          $('#edit-field-main-image-0-ahah-wrapper, div#field-images-items, ' +
-            '#edit-field-posting-type-value-wrapper').show();
-          $('div#field-images-items').css('display', 'inline');
-          description.html('This is where you add details that are specific to the item you own. <br /> For example: Condition, Damage, History etc.');
-        }
-      })
-    
-      //Only show offer2buy if the item is marked for sale
-      var postingType = $('select#edit-field-posting-type-value').val();
+		textarea.blur(function() {
+			if(textarea.val() == '') {
+				description.show();
+			}
+		});
+		//only apply to the item step
+		if($('body.page-product-new-item').is('*')) {
+			var initRadioValue = $('input[name=have_want]:checked').val();
 
-      if(postingType == 1){
-        $('div#edit-o2b-price-wrapper').parent().hide();
-      }
+			//If we're in the "want" mode
+			if(initRadioValue == 1) {
+				//...hide elements
+				$('#edit-field-main-image-0-ahah-wrapper, ' + '#edit-field-posting-type-value-wrapper').hide();
+				$('div#field-images-items').css('display', 'none');
+				$('div#edit-o2b-price-wrapper').parent().hide();
+				//Set the body description
+				description.html('Why do you want this item?');
+			}
 
-      //If offer2buy is toggled on, show the price field.
-      $('select#edit-field-posting-type-value').click(function (){
-        if($('select#edit-field-posting-type-value').val() == 2){
-          $('div#edit-o2b-price-wrapper').parent().show();
-        }else{
-          $('div#edit-o2b-price-wrapper').parent().hide();
-        }
-      })
-    }
-  }
-})
+			$('div.form-radios').click(function() {
+				var radioValue = $('input[name=have_want]:checked').val();
+				//If it's a want form...
+				if(radioValue == 1) {
+					//...hide elements
+					$('#edit-field-main-image-0-ahah-wrapper, div#field-images-items, ' + '#edit-field-posting-type-value-wrapper').hide();
+					$('div#field-images-items').css('display', 'none');
+					$('div#edit-o2b-price-wrapper').parent().hide();
+					description.html('Why do you want this item?');
+					//move to the right column
+					$('#edit-privacy-setting-wrapper').addClass('form-right');
+					$('fieldset').addClass('fieldset-right');
+				} else {
+					$('#edit-field-main-image-0-ahah-wrapper, div#field-images-items, ' + '#edit-field-posting-type-value-wrapper').show();
+					$('div#field-images-items').css('display', 'inline');
+					description.html('This is where you add details that are specific to the item you own. For example: Condition, Damage, History etc.');
+					//remove from the right column
+					$('#edit-privacy-setting-wrapper').removeClass('form-right');
+					$('fieldset').removeClass('fieldset-right');
+				}
+			})
+			//Only show offer2buy if the item is marked for sale
+			var postingType = $('select#edit-field-posting-type-value').val();
+
+			if(postingType == 1) {
+				$('div#edit-o2b-price-wrapper').parent().hide();
+			}
+
+			//If offer2buy is toggled on, show the price field.
+			$('select#edit-field-posting-type-value').click(function() {
+				if($('select#edit-field-posting-type-value').val() == 2) {
+					$('div#edit-o2b-price-wrapper').parent().show();
+				} else {
+					$('div#edit-o2b-price-wrapper').parent().hide();
+				}
+			})
+		}
+	}
+});

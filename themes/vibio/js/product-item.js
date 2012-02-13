b2f16776fa891ac20f117d8660578abf23cb116a
Craig Tockman <craigtockman@gmail.com>
2012-02-13 15:45:25 -0800

    have want js toggle

--- themes/vibio/js/product-item.js
+++ themes/vibio/js/product-item.js
@@ -65,13 +65,14 @@ $(document).ready(function() {
 					$('div#edit-o2b-price-wrapper').parent().hide();
 					description.html('Why do you want this item?');
 					//move to the right column
-					//$('#edit-privacy-setting-wrapper').addClass('form-right');
-					//$('fieldset').addClass('fieldset-right');
+					$('#edit-privacy-setting-wrapper').insertBefore('#form-column-right #field-images-items');
+					$('#collection-fieldset').insertBefore('#form-column-right #edit-field-main-image-0-ahah-wrapper');
 				} else {
 					$('#edit-field-main-image-0-ahah-wrapper, div#field-images-items, ' + '#edit-field-posting-type-value-wrapper').show();
 					description.html('This is where you add details that are specific to the item you own. For example: Condition, Damage, History etc.');
 					//remove from the right column
-					//$('#edit-privacy-setting-wrapper').removeClass('form-right');
+					$('#edit-privacy-setting-wrapper').insertAfter('#form-price');
+					$('#collection-fieldset').insertAfter('#edit-privacy-setting-wrapper');
 					//$('fieldset').removeClass('fieldset-right');
 				}
 			})

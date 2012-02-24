f726fe017eba89f3f70247438efd279519891915
Craig Tockman <craigtockman@gmail.com>
2012-02-23 22:16:40 -0800

    qa fine tuning

--- themes/vibio/js/fixed-scroll.js
+++ themes/vibio/js/fixed-scroll.js
@@ -1,17 +1,24 @@
 /*fixed scroll*/
+
 $(document).ready(function() {
 	$('#block-block-5').scrollToFixed();
+	$('#splash-fb-bgd').scrollToFixed();
+	
 	$('#block-block-5').bind('fixed', function() {
 		$('.logged-in .sidebar .rounded_content').css('height', '400px');
 	});
 	$('#block-block-5').bind('unfixed', function() {
-		$(this).css('position', 'absolute');
+		
 		$('.logged-in .sidebar .rounded_content').css('height', 'auto');
 	});
-	//$('.view.view-flag-featured .view-footer').scrollToFixed();
-});
+	
+
+
 /*scroll to top*/
+
 $('a.topOfPage').click(function() {
 	$.scrollTo(0, 500);
 	return false;
+});
+
 });
